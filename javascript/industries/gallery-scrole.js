b36eeb6359bc78ae50f4cd0cd9519db747775dda
VDaily <workingemail2207@gmail.com>
2022-07-29 18:57:41 +0300

    Working on the 'Industries' section; Making a slider version - 0.3

--- javascript/industries/gallery-scrole.js
+++ javascript/industries/gallery-scrole.js
@@ -1,38 +1,54 @@
 window.addEventListener("load", function () {
   let arrowLeft = document.querySelector(".gallery-slider__arrow-left");
   let arrowRight = document.querySelector(".gallery-slider__arrow-right");
+  let groupItems = document.querySelector(".gallery-slider__group-items");
+  let galleryItems = document.querySelectorAll(".gallery-slider__item");
   let translateXElem = 0;
+  let index = 0;
 
   if (!arrowLeft && !arrowRight) throw new Error("Не найдены элементы arrow");
 
   arrowLeft.addEventListener("click", function (event) {
-    console.log("left");
+    let bigItem = document.querySelector(".gallery-slider__item_size-more");
+    let previousElement = bigItem.previousElementSibling;
+    let marginRight, widthElement;
+
+    if (!groupItems) return;
+    if (!bigItem) return;
+
+    marginRight = parseFloat(getComputedStyle(previousElement).marginRight);
+    widthElement = previousElement.offsetWidth + marginRight;
+    console.log(widthElement);
+    console.log(previousElement.offsetWidth, marginRight, widthElement);
+    translateXElem = translateXElem + widthElement;
+    groupItems.style.transform = `translateX(${translateXElem + "px"})`;
+
+    previousElement.classList.add("gallery-slider__item_size-more");
+    bigItem.classList.remove("gallery-slider__item_size-more");
   });
 
   arrowRight.addEventListener("click", function (event) {
-    let groupItems = document.querySelector(".gallery-slider__group-items");
     let bigItem = document.querySelector(".gallery-slider__item_size-more");
-
+    let count = galleryItems.length;
+    console.log(count);
+    let marginRight, widthElement, nextElement;
+    nextElement = bigItem.nextElementSibling;
+    console.log(galleryItems);
     if (!groupItems) return;
-    let marginRight, widthElement, heightElement;
+
     if (!bigItem) return;
 
-    marginRight = parseFloat(
-      getComputedStyle(groupItems.children[0]).marginRight
-    );
-    widthElement = groupItems.children[0].offsetWidth + marginRight;
-    heightElement = groupItems.children[0].offsetHeight;
+    marginRight = parseFloat(getComputedStyle(nextElement).marginRight);
+    widthElement = nextElement.offsetWidth + marginRight;
+    console.log(widthElement);
+    console.log(nextElement.offsetWidth, marginRight, widthElement);
     translateXElem = translateXElem - widthElement;
-
     console.log(translateXElem);
+
     groupItems.style.transform = `translateX(${translateXElem + "px"})`;
-    groupItems.style.tranfform = `translateY9(${heightElement + "px"})`;
-    let nextElement = bigItem.nextElementSibling;
-
-    nextElement.style.width = translateXElem + "px";
-    // nextElement.style.transition = `width 2s linear, margin-top 2s linear`;
-
     nextElement.classList.add("gallery-slider__item_size-more");
     bigItem.classList.remove("gallery-slider__item_size-more");
   });
 });
+
+class Gallery {}
