f68651a969a313c2f4bbbb8c5d6213f2b4a660f8
VDaily <workingemail2207@gmail.com>
2022-08-08 19:22:18 +0300

    replaced "margin-top" with "margin-bottom"      and "margin-left" with "margin-right"; added some media queries

--- javascript/industries/gallery-scrole.js
+++ javascript/industries/gallery-scrole.js
@@ -1,33 +1,40 @@
-window.addEventListener("load", function () {
-  let arrowLeft = document.querySelector(".gallery-slider__arrow-left");
-  let arrowRight = document.querySelector(".gallery-slider__arrow-right");
-  let groupItems = document.querySelector(".gallery-slider__group-items");
-  let galleryItems = document.querySelectorAll(".gallery-slider__item");
-  let translateXElem = 0;
-  let index = 0;
+let arrowLeft = document.querySelector(".gallery-slider__arrow-left");
+let arrowRight = document.querySelector(".gallery-slider__arrow-right");
+let groupItems = document.querySelector(".gallery-slider__group-items");
+let galleryItems = document.querySelectorAll(".gallery-slider__item");
+let gallerySlider = document.querySelector(".gallery-slider");
+let galleryHeightMax;
+let translateXElem = 0;
 
-  if (!arrowLeft && !arrowRight) throw new Error("Не найдены элементы arrow");
+let index = 0;
 
-  arrowLeft.addEventListener("click", function (event) {
+if (!arrowLeft && !arrowRight) throw new Error("Не найдены элементы arrow");
+
+arrowLeft.addEventListener(
+  "click",
+  function (event) {
     let bigItem = document.querySelector(".gallery-slider__item_size-more");
     let previousElement = bigItem.previousElementSibling;
+
     let marginRight, widthElement;
 
     if (!groupItems) return;
     if (!bigItem) return;
-
     marginRight = parseFloat(getComputedStyle(previousElement).marginRight);
     widthElement = previousElement.offsetWidth + marginRight;
-    console.log(widthElement);
-    console.log(previousElement.offsetWidth, marginRight, widthElement);
-    translateXElem = translateXElem + widthElement;
-    groupItems.style.transform = `translateX(${translateXElem + "px"})`;
 
+    translateXElem = translateXElem + widthElement;
+
+    groupItems.style.transform = `translateX(${translateXElem / 100 + "%"})`;
     previousElement.classList.add("gallery-slider__item_size-more");
     bigItem.classList.remove("gallery-slider__item_size-more");
-  });
+  },
+  { passive: true }
+);
 
-  arrowRight.addEventListener("click", function (event) {
+arrowRight.addEventListener(
+  "click",
+  function (event) {
     let bigItem = document.querySelector(".gallery-slider__item_size-more");
     let count = galleryItems.length;
     console.log(count);
@@ -40,15 +47,13 @@ window.addEventListener("load", function () {
 
     marginRight = parseFloat(getComputedStyle(nextElement).marginRight);
     widthElement = nextElement.offsetWidth + marginRight;
-    console.log(widthElement);
-    console.log(nextElement.offsetWidth, marginRight, widthElement);
-    translateXElem = translateXElem - widthElement;
-    console.log(translateXElem);
 
-    groupItems.style.transform = `translateX(${translateXElem + "px"})`;
+    translateXElem = translateXElem - widthElement;
+
+    groupItems.style.transform = `translateX(${translateXElem / 100 + "%"})`;
     nextElement.classList.add("gallery-slider__item_size-more");
     bigItem.classList.remove("gallery-slider__item_size-more");
-  });
-});
-
+  },
+  { passive: true }
+);
 class Gallery {}
--- javascript/window/scroll.js
+++ javascript/window/scroll.js
@@ -1,31 +1,44 @@
 let header = document.querySelector(".header__top");
-let heightScroll = 0;
-let marginTop = parseFloat(getComputedStyle(header, null).marginTop);
-headerStyle();
+let headerMarginTop = parseFloat(getComputedStyle(header, null).marginTop);
+fixingHeaderWhenScrolling();
 
-window.addEventListener("scroll", headerStyle);
+window.addEventListener("scroll", fixingHeaderWhenScrolling);
 
-function headerStyle(event) {
-  if (window.pageYOffset > marginTop) {
+function fixingHeaderWhenScrolling(event) {
+  let NumberOfPixelsFromTop = window.pageYOffset;
+  if (NumberOfPixelsFromTop > headerMarginTop) {
     header.classList.add("header__top_retrofit");
   } else {
-      header.classList.remove("header__top_retrofit");
+    header.classList.remove("header__top_retrofit");
   }
 }
 
-header.addEventListener('click', function clickAnchor(event) {
-  let link = event.target.closest('a')
-  if(!link) return;
+header.addEventListener("click", function scrollingToLinkedElement(event) {
+  let link = event.target.closest(".nav__link");
+  let id,
+    linkedElement,
+    oldActiveLink,
+    distanceOfElementFromTop,
+    headerHeight,
+    distanceWithoutHeaderHeight;
 
-  let href = link.getAttribute('href').slice(1);
-  let element = document.getElementById(href);
+  if (!link) return;
 
-  if(!element) return;
-  let oldLinkActive = document.querySelector('.nav__link_active');
-  oldLinkActive.classList.remove('nav__link_active');
-  link.classList.add("nav__link_active");
-  let heightY = element.getBoundingClientRect().top;
-  let hieghtHeader = header.offsetHeight;
+  id = link.getAttribute("href").slice(1);
+  linkedElement = document.getElementById(id);
+
+  if (!linkedElement) return;
   event.preventDefault();
-  window.scrollBy({top: heightY - hieghtHeader, behavior: "smooth"});
+
+  oldActiveLink = document.querySelector(".nav__link_active");
+  oldActiveLink.classList.remove("nav__link_active");
+  link.classList.add("nav__link_active");
+  distanceOfElementFromTop = linkedElement.getBoundingClientRect().top;
+  headerHeight = header.offsetHeight;
+  distanceWithoutHeaderHeight = distanceOfElementFromTop - headerHeight;
+
+  window.scrollBy({
+    top: distanceWithoutHeaderHeight,
+    behavior: "smooth",
+  });
 });
